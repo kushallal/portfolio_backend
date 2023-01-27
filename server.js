f1a151ba96b58986c7fe9aeff6cd0db18658b1a8
Kushal Lal Shrestha <kushallalshrestha@rocketmail.com>
2023-01-27 19:10:40 +0545

    port added

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-
+const PORT = 3000;
 app.get("/test", (req, res) => {
   res.json({ test: "test message" });
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("server is running ...");
 });
